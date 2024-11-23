831d079c7373db6f3603d40c377760d4df47797a
Thanyaluk Jirapech-umpai <thanyaluk.jirapechumpai@fanduel.com>
2024-11-23 19:19:44 +0000

    fix unit test

--- src/test-action/__tests__/index.test.ts
+++ src/test-action/__tests__/index.test.ts
@@ -2,15 +2,15 @@
  * Unit tests for the action's entrypoint, src/index.ts
  */
 
-import * as main from '../src/main'
+import * as testAction from '../test-action'
 
 // Mock the action's entrypoint
-const runMock = jest.spyOn(main, 'run').mockImplementation()
+const runMock = jest.spyOn(testAction, 'run').mockImplementation()
 
 describe('index', () => {
   it('calls run when imported', () => {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
-    require('../src/index')
+    require('../index')
 
     expect(runMock).toHaveBeenCalled()
   })
--- src/test-action/index.ts
+++ src/test-action/index.ts
@@ -3,7 +3,4 @@
  */
 import { run } from './test-action'
 
-run().catch(error => {
-  console.error(error)
-  process.exit(1)
-})
+run()
