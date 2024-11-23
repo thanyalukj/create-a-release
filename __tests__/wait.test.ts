319d56b7a71c18d447d1486356ebc9540f16fce3
Thanyaluk Jirapech-umpai <thanyaluk.jirapechumpai@fanduel.com>
2024-11-23 18:52:29 +0000

    fix import in tests

--- __tests__/wait.test.ts
+++ __tests__/wait.test.ts
@@ -2,7 +2,7 @@
  * Unit tests for src/wait.ts
  */
 
-import { wait } from '../src/wait'
+import { wait } from '../src/test-action/wait'
 import { expect } from '@jest/globals'
 
 describe('wait.ts', () => {
