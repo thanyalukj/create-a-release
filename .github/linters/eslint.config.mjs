47dabc40868175b32104de3e40193c1ade4dd41f
Thanyaluk Jirapech-umpai <thanyaluk.jirapechumpai@fanduel.com>
2024-11-23 14:11:58 +0000

    fix formating in eslint.config.mjs

--- .github/linters/eslint.config.mjs
+++ .github/linters/eslint.config.mjs
@@ -1,91 +1,101 @@
-import jest from "eslint-plugin-jest";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import jest from 'eslint-plugin-jest'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
 
-export default [{
+export default [
+  {
     ignores: [
-        "!**/.*",
-        "**/node_modules/.*",
-        "**/*.json",
-        "**/coverage/*",
-        "**/dist/*",
-        "**/__tests__/*",
-        "**/.github/*",
-        "**/linters/*",
-        "eslint.config.mjs",
-    ],
-}, ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:jest/recommended",
-), {
+      '!**/.*',
+      '**/node_modules/.*',
+      '**/*.json',
+      '**/coverage/*',
+      '**/dist/*',
+      '**/__tests__/*',
+      '**/.github/*',
+      '**/linters/*',
+      'eslint.config.mjs'
+    ]
+  },
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:jest/recommended'
+  ),
+  {
     plugins: {
-        jest,
-        "@typescript-eslint": typescriptEslint,
+      jest,
+      '@typescript-eslint': typescriptEslint
     },
 
     languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.jest,
-            Atomics: "readonly",
-            SharedArrayBuffer: "readonly",
-        },
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+      },
 
-        parserOptions: {
-            parser: tsParser,
-            ecmaVersion: 2023,
-            sourceType: "module",
-            warnOnUnsupportedTypeScriptVersion: false,
-            project: ["./.github/linters/tsconfig.json", "./tsconfig.json"],
-        },
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: 2023,
+        sourceType: 'module',
+        warnOnUnsupportedTypeScriptVersion: false,
+        project: ['./.github/linters/tsconfig.json', './tsconfig.json']
+      }
     },
 
     rules: {
-        camelcase: "off",
-        "eslint-comments/no-use": "off",
-        "eslint-comments/no-unused-disable": "off",
-        "i18n-text/no-en": "off",
-        "import/no-namespace": "off",
-        "no-console": "off",
-        semi: "off",
-        "@typescript-eslint/array-type": "error",
-        "@typescript-eslint/consistent-type-assertions": "error",
+      camelcase: 'off',
+      'eslint-comments/no-use': 'off',
+      'eslint-comments/no-unused-disable': 'off',
+      'i18n-text/no-en': 'off',
+      'import/no-namespace': 'off',
+      'no-console': 'off',
+      semi: 'off',
+      '@typescript-eslint/array-type': 'error',
+      '@typescript-eslint/consistent-type-assertions': 'error',
 
-        "@typescript-eslint/explicit-member-accessibility": ["error", {
-            accessibility: "no-public",
-        }],
+      '@typescript-eslint/explicit-member-accessibility': [
+        'error',
+        {
+          accessibility: 'no-public'
+        }
+      ],
 
-        "@typescript-eslint/explicit-function-return-type": ["error", {
-            allowExpressions: true,
-        }],
+      '@typescript-eslint/explicit-function-return-type': [
+        'error',
+        {
+          allowExpressions: true
+        }
+      ],
 
-        "@typescript-eslint/no-empty-interface": "error",
-        "@typescript-eslint/no-extraneous-class": "error",
-        "@typescript-eslint/no-inferrable-types": "error",
-        "@typescript-eslint/no-non-null-assertion": "warn",
-        "@typescript-eslint/no-unnecessary-qualifier": "error",
-        "@typescript-eslint/no-useless-constructor": "error",
-        "@typescript-eslint/no-var-requires": "error",
-        "@typescript-eslint/prefer-for-of": "warn",
-        "@typescript-eslint/prefer-function-type": "warn",
-        "@typescript-eslint/prefer-includes": "error",
-        "@typescript-eslint/prefer-string-starts-ends-with": "error",
-        "@typescript-eslint/promise-function-async": "error",
-        "@typescript-eslint/require-array-sort-compare": "error",
-        "@typescript-eslint/space-before-function-paren": "off",
-    },
-}];
+      '@typescript-eslint/no-empty-interface': 'error',
+      '@typescript-eslint/no-extraneous-class': 'error',
+      '@typescript-eslint/no-inferrable-types': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+      '@typescript-eslint/no-unnecessary-qualifier': 'error',
+      '@typescript-eslint/no-useless-constructor': 'error',
+      '@typescript-eslint/no-var-requires': 'error',
+      '@typescript-eslint/prefer-for-of': 'warn',
+      '@typescript-eslint/prefer-function-type': 'warn',
+      '@typescript-eslint/prefer-includes': 'error',
+      '@typescript-eslint/prefer-string-starts-ends-with': 'error',
+      '@typescript-eslint/promise-function-async': 'error',
+      '@typescript-eslint/require-array-sort-compare': 'error',
+      '@typescript-eslint/space-before-function-paren': 'off'
+    }
+  }
+]
